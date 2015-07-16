5bb4960a705113df175c10b19c15dec91d2368ea
csjc008 <csjc008@163.com>
2015-07-16 17:23:47 +0800

    add prbm5/Solution.java

---
+++ src/prbm5/Solution.java
@@ -0,0 +1,47 @@
+package prbm5;
+
+public class Solution {
+    public String longestPalindrome(String s) {
+        int len = s.length();
+        int maxl = 0;
+        int maxr = 0;
+        int maxLen = 1;
+        for (int k = 0; k < len * 2 - 1; k++) {
+            int base = 0;
+            int plen = 1;
+            int left = k / 2;
+            int right = k / 2;
+            if (k % 2 == 1) {
+                base = 1;
+                plen = 0;
+                left = (k - 1) / 2;
+                right = (k + 1) / 2;
+            }
+            for (int i = base; k - i >= 0 && (k + i) / 2 <= len - 1; i = i + 2) {
+                int l = (k - i) / 2;
+                int r = (k + i) / 2;
+                if (s.charAt(l) != s.charAt(r)) {
+                    break;
+                }
+                left = l;
+                right = r;
+                plen = r - l + 1;
+            }
+            if (plen > maxLen) {
+                maxl = left;
+                maxr = right;
+                maxLen = plen;
+            }
+        }
+        return s.substring(maxl, maxr + 1);
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.longestPalindrome("a"));
+        System.out.println(s.longestPalindrome("aba"));
+        System.out.println(s.longestPalindrome("aa"));
+        System.out.println(s.longestPalindrome("abac"));
+        System.out.println(s.longestPalindrome("baab"));
+    }
+}
