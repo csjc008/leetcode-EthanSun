41858c545c34abd8e71dd02d25eccb59dca7d929
yahao.sun <csjc008@163.com>
2016-12-23 00:07:25 +0800

    submit prbm4

--- src/prbm4/Solution.java
+++ src/prbm4/Solution.java
@@ -21,7 +21,7 @@ import java.util.List;
 public class Solution {
 
     /**
-     * Time Complexity: O(logm)*O(logn)
+     * AC Time Complexity: O(logm)*O(logn)
      */
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int l1 = nums1.length;
