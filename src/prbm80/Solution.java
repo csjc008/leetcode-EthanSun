561be5de3dec95c0f1e2d0a4cb5e7111d2c5264a
yahao.sun <yahao.sun@qunar.com>
2017-01-24 17:41:35 +0800

    ac

--- src/prbm80/Solution.java
+++ src/prbm80/Solution.java
@@ -4,15 +4,18 @@ package prbm80;
  * 80. <b>Remove Duplicates from Sorted Array II</b><br>
  * Follow up for "Remove Duplicates":<br>
  * What if duplicates are allowed at most twice?<br>
- * 
+ *
  * For example,<br>
  * Given sorted array nums = [1,1,1,2,2,3],<br>
- * 
+ *
  * Your function should return length = 5, with the first five elements of nums
  * being 1, 1, 2, 2 and 3. It doesn't matter what you leave beyond the new
  * length.<br>
  */
 public class Solution {
+    /**
+     * AC
+     */
     public int removeDuplicates(int[] nums) {
         Integer current = null;
         Integer prev = null;
